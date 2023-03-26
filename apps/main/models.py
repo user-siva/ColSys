9d458b2fe4f28f0efd39c83ddf061964239d1b1c
user-siva <sivag0503@gmail.com>
2023-03-26 12:11:03 +0530

    student attendance init

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+
+year_choices = [
+    ('First', 'First'),
+    ('Second', 'Second'),
+    ('Third', 'Third'),
+    ('Fourth', 'Fourth'),
+]
+
 dept_choices = [
     ('Computer Science and Engineering', 'CSE'),
     ('Electronics and Communication Engineering', 'ECE'),
@@ -33,55 +41,59 @@ transport_choice = [
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=200)
-    Admission_number = models.IntegerField(null=True)
-    Admission_date = models.DateField(null=True)
-    Batch_name = models.CharField(max_length=20, null=True)
-    Register = models.IntegerField(null=True)
-    Roll_no = models.IntegerField(null=True)
+    name = models.CharField(max_length=200, null=True)
+    Admission_number = models.IntegerField(null=True, blank=True)
+    Admission_date = models.DateField(null=True, blank=True)
+    Batch_name = models.CharField(max_length=20, null=True, blank=True)
+    Register = models.IntegerField(null=True, blank=True)
+    Roll_no = models.IntegerField(null=True, blank=True)
     Age = models.IntegerField(null=True)
-    DOB = models.DateField(null=True)
+    DOB = models.DateField(null=True, blank=True)
     Gender = models.CharField(max_length=10, choices=gender_choices, null=True)
-    Blood_group = models.CharField(max_length=5, null=True)
+    Blood_group = models.CharField(max_length=5, null=True, blank=True)
     Department = models.CharField(max_length=200, choices=dept_choices)
-    Year = models.IntegerField(null=True)
+    year = models.CharField(
+        max_length=200, default='First', choices=year_choices, null=True)
     Phone = models.IntegerField(null=True)
-    email = models.CharField(max_length=200, null=True)
-    Aadhaar_no = models.IntegerField(null=True)
-    Father_name = models.CharField(max_length=200)
-    Father_phone = models.IntegerField(null=True)
-    Occupation = models.CharField(max_length=50, null=True)
-    Mother_name = models.CharField(max_length=200)
-    Mother_phone = models.IntegerField(null=True)
-    Mother_Occupation = models.CharField(max_length=50, null=True)
-    Annual_income = models.IntegerField(null=True)
-    Nationality = models.CharField(max_length=10, null=True)
-    Religion = models.CharField(max_length=30, null=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    Aadhaar_no = models.IntegerField(null=True, blank=True)
+    Father_name = models.CharField(max_length=200, blank=True)
+    Father_phone = models.IntegerField(null=True, blank=True)
+    Occupation = models.CharField(max_length=50, null=True, blank=True)
+    Mother_name = models.CharField(max_length=200, blank=True)
+    Mother_phone = models.IntegerField(null=True, blank=True)
+    Mother_Occupation = models.CharField(max_length=50, null=True, blank=True)
+    Annual_income = models.IntegerField(null=True, blank=True)
+    Nationality = models.CharField(max_length=10, null=True, blank=True)
+    Religion = models.CharField(max_length=30, null=True, blank=True)
     Student_category = models.CharField(
-        max_length=10, choices=category_choices, null=True)
-    Door_no = models.IntegerField(null=True)
-    Street_and_Area = models.CharField(max_length=200, null=True)
-    District = models.CharField(max_length=50, null=True)
-    State = models.CharField(max_length=50, null=True)
-    Country = models.CharField(max_length=50, null=True)
-    Pincode = models.IntegerField(null=True)
+        max_length=10, choices=category_choices, null=True, blank=True)
+    Door_no = models.IntegerField(null=True, blank=True)
+    Street_and_Area = models.CharField(max_length=200, null=True, blank=True)
+    District = models.CharField(max_length=50, null=True, blank=True)
+    State = models.CharField(max_length=50, null=True, blank=True)
+    Country = models.CharField(max_length=50, null=True, blank=True)
+    Pincode = models.IntegerField(null=True, blank=True)
     Mode_of_Transpotation = models.CharField(
-        max_length=50, choices=transport_choice, null=True)
-    Bus_route_no = models.IntegerField(null=True)
-    Rugular_Boarding_point = models.CharField(max_length=50, null=True)
-    Regular_dropping_point = models.CharField(max_length=50, null=True)
-    Guardian_name = models.CharField(max_length=50, null=True)
+        max_length=50, choices=transport_choice, null=True, blank=True)
+    Bus_route_no = models.IntegerField(null=True, blank=True)
+    Rugular_Boarding_point = models.CharField(
+        max_length=50, null=True, blank=True)
+    Regular_dropping_point = models.CharField(
+        max_length=50, null=True, blank=True)
+    Guardian_name = models.CharField(max_length=50, null=True, blank=True)
     Guardian_is = models.CharField(
-        max_length=20, choices=guardian_choice, null=True)
-    Guardian_mobile = models.IntegerField(null=True)
-    Guardian_Address = models.CharField(max_length=300, null=True)
-    Guardian_2_name = models.CharField(max_length=50, null=True)
+        max_length=20, choices=guardian_choice, null=True, blank=True)
+    Guardian_mobile = models.IntegerField(null=True, blank=True)
+    Guardian_Address = models.CharField(max_length=300, null=True, blank=True)
+    Guardian_2_name = models.CharField(max_length=50, null=True, blank=True)
     Guardian_2_is = models.CharField(
-        max_length=20, choices=guardian_choice, null=True)
-    Guardian_2_mobile = models.IntegerField(null=True)
-    Guardian_2_Address = models.CharField(max_length=300, null=True)
+        max_length=20, choices=guardian_choice, null=True, blank=True)
+    Guardian_2_mobile = models.IntegerField(null=True, blank=True)
+    Guardian_2_Address = models.CharField(
+        max_length=300, null=True, blank=True)
     Emergency_contact = models.CharField(
-        max_length=30, choices=guardian_choice, null=True)
+        max_length=30, choices=guardian_choice, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -164,13 +176,6 @@ sem_choices = [
     ('Second', 'Second'),
 ]
 
-year_choices = [
-    ('First', 'First'),
-    ('Second', 'Second'),
-    ('Third', 'Third'),
-    ('Fourth', 'Fourth'),
-]
-
 
 class Subject(models.Model):
     subject_code = models.CharField(max_length=100)
@@ -218,3 +223,27 @@ class TimeTable(models.Model):
 
     def __str__(self):
         return self.day+' '+self.period+' '+' period'
+
+
+att_choice = [
+    ('Present', 'Present'),
+    ('Absent', 'Absent'),
+    ('On Duty', 'On Duty')
+]
+
+
+class StudentAttendance(models.Model):
+    name = models.ForeignKey(Student, on_delete=models.CASCADE)
+    day = models.CharField(max_length=100, null=True, choices=day_choice)
+    date = models.DateField()
+    time = models.TimeField()
+    period_1 = models.CharField(max_length=100, choices=att_choice, null=True)
+    period_2 = models.CharField(max_length=100, choices=att_choice, null=True)
+    period_3 = models.CharField(max_length=100, choices=att_choice, null=True)
+    period_4 = models.CharField(max_length=100, choices=att_choice, null=True)
+    period_5 = models.CharField(max_length=100, choices=att_choice, null=True)
+    period_6 = models.CharField(max_length=100, choices=att_choice, null=True)
+    period_7 = models.CharField(max_length=100, choices=att_choice, null=True)
+    period_8 = models.CharField(max_length=100, choices=att_choice, null=True)
+    reason = models.CharField(max_length=500, null=True, blank=True)
+    mark_attendance = models.CharField(max_length=100, choices=att_choice)
