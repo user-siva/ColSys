d8f8009797fc64e8958d223ffae190f7e3d69602
user-siva <sivag0503@gmail.com>
2023-03-26 12:10:21 +0530

    student attendance init

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -76,7 +76,54 @@ class TimeTableAdmin(admin.ModelAdmin):
             return super().changelist_view(request, extra_context=extra_context)
 
 
+class StudentAttendanceAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/attend.html'
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        values = False
+        if request.method == 'POST':
+            dept = request.POST.get('department')
+            year = request.POST.get('year')
+            values = True
+            if dept == 'CSE':
+                dept = 'Computer Science and Engineering'
+            elif dept == 'ECE':
+                dept = 'Electronics and Communication Engineering'
+            elif dept == 'EEE':
+                dept = 'Electrical and Electronic Engineering'
+            elif dept == 'Mech':
+                dept = 'Mechanical Engineering'
+            elif dept == 'Civil':
+                dept = 'Civil Engineering'
+            monday = TimeTable.objects.filter(
+                day="Monday", subject_name__department=dept, subject_name__year=year)
+            tuesday = TimeTable.objects.filter(
+                day="Tuesday", subject_name__department=dept, subject_name__year=year)
+            wednesday = TimeTable.objects.filter(
+                day="Wednesday", subject_name__department=dept, subject_name__year=year)
+            thursday = TimeTable.objects.filter(
+                day="Thursday", subject_name__department=dept, subject_name__year=year)
+            friday = TimeTable.objects.filter(
+                day="Friday", subject_name__department=dept, subject_name__year=year)
+            sub = Subject.objects.filter(department=dept, year=year)
+            extra_context.update({
+                "monday": monday,
+                'tuesday': tuesday,
+                'wednesday': wednesday,
+                'thursday': thursday,
+                'friday': friday,
+                'sub': sub,
+                'values': values
+            })
+
+            return super().changelist_view(request, extra_context=extra_context)
+        else:
+            return super().changelist_view(request, extra_context=extra_context)
+
+
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Staff, StaffAdmin)
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(TimeTable, TimeTableAdmin)
+admin.site.register(StudentAttendance, StudentAttendanceAdmin)
